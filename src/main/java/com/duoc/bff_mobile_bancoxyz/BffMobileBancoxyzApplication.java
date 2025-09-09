c9bc41733ef11cb39ad8e2b4c77762b208201f5b
LeoOlivaresD <cspraword@gmail.com>
2025-09-09 00:50:11 -0300

    Commit inicial para bff movil

---
+++ src/main/java/com/duoc/bff_mobile_bancoxyz/BffMobileBancoxyzApplication.java
@@ -0,0 +1,13 @@
+package com.duoc.bff_mobile_bancoxyz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BffMobileBancoxyzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BffMobileBancoxyzApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/duoc/bff_mobile_bancoxyz/BffMobileBancoxyzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.duoc.bff_mobile_bancoxyz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BffMobileBancoxyzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
