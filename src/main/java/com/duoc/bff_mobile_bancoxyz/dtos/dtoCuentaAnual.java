b05a81650cc1e7a2f3ed27502a26d4d216e25524
LeoOlivaresD <cspraword@gmail.com>
2025-09-09 01:15:51 -0300

    Creacion de dto cuenta anual

--- src/main/java/com/duoc/bff_mobile_bancoxyz/dtos/dtoCuentaAnual.java
+++ src/main/java/com/duoc/bff_mobile_bancoxyz/dtos/dtoCuentaAnual.java
@@ -1,5 +1,16 @@
 package com.duoc.bff_mobile_bancoxyz.dtos;
 
-public class DtoCuentaAnual {
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class DtoCuentaAnual {
+    private String idCuenta;
+    private Integer monto;
+    private String tipoTransaccion;
 }
